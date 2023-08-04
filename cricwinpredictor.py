807eb0f5615756a075b469432b3b2a4eae335b4c
Snehal Sobti <106326726+snehalsobti@users.noreply.github.com>
2023-08-04 09:46:03 -0400

    Update cricwinpredictor.py

--- cricwinpredictor.py
+++ cricwinpredictor.py
@@ -19,65 +19,65 @@ with st.sidebar:
     st.subheader("by Krishna Advait Sripada, Katarina Poffley, Snehal Sobti, Lianne Choong")
     st.caption("© 2023 by Crickentron")
 
-# team1_player1 = 'SR Tendulkar'
-# team1_player2 = 'ST Jayasuriya'
-# team1_player3 = 'R Dravid'
-# team1_player4 = 'RT Ponting'
-# team1_player5 = 'KC Sangakkara'
-# team1_player6 = 'MS Dhoni'
-# team1_player7 = 'JH Kallis'
-# team1_player8 = 'M Muralidaran'
-# team1_player9 = 'B Lee'
-# team1_player10 = 'A Kumble'
-# team1_player11 = 'Wasim Akram'
+# team0_player1 = 'SR Tendulkar'
+# team0_player2 = 'ST Jayasuriya'
+# team0_player3 = 'R Dravid'
+# team0_player4 = 'RT Ponting'
+# team0_player5 = 'KC Sangakkara'
+# team0_player6 = 'MS Dhoni'
+# team0_player7 = 'JH Kallis'
+# team0_player8 = 'M Muralidaran'
+# team0_player9 = 'B Lee'
+# team0_player10 = 'A Kumble'
+# team0_player11 = 'Wasim Akram'
 
-# team2_player1 = 'PP Shaw'
-# team2_player2 = 'RD Gaikwad'
-# team2_player3 = 'V Shankar'
-# team2_player4 = 'KM Jadhav'
-# team2_player5 = 'DJ Hooda'
-# team2_player6 = 'NV Ojha'
-# team2_player7 = 'MA Agarwal'
-# team2_player8 = 'Ravi Bishnoi'
-# team2_player9 = 'DL Chahar'
-# team2_player10 = 'RD Chahar'
-# team2_player11 = 'JD Unadkat'
+# team1_player1 = 'PP Shaw'
+# team1_player2 = 'RD Gaikwad'
+# team1_player3 = 'V Shankar'
+# team1_player4 = 'KM Jadhav'
+# team1_player5 = 'DJ Hooda'
+# team1_player6 = 'NV Ojha'
+# team1_player7 = 'MA Agarwal'
+# team1_player8 = 'Ravi Bishnoi'
+# team1_player9 = 'DL Chahar'
+# team1_player10 = 'RD Chahar'
+# team1_player11 = 'JD Unadkat'
+
+st.subheader("Enter Team 0 players:")
+
+team0_player1 = st.text_input("Player 1")
+team0_player2 = st.text_input("Player 2")
+team0_player3 = st.text_input("Player 3")
+team0_player4 = st.text_input("Player 4")
+team0_player5 = st.text_input("Player 5")
+team0_player6 = st.text_input("Player 6")
+team0_player7 = st.text_input("Player 7")
+team0_player8 = st.text_input("Player 8")
+team0_player9 = st.text_input("Player 9")
+team0_player10 = st.text_input("Player 10")
+team0_player11 = st.text_input("Player 11")
 
 st.subheader("Enter Team 1 players:")
 
-team1_player1 = st.text_input("Player 1")
-team1_player2 = st.text_input("Player 2")
-team1_player3 = st.text_input("Player 3")
-team1_player4 = st.text_input("Player 4")
-team1_player5 = st.text_input("Player 5")
-team1_player6 = st.text_input("Player 6")
-team1_player7 = st.text_input("Player 7")
-team1_player8 = st.text_input("Player 8")
-team1_player9 = st.text_input("Player 9")
-team1_player10 = st.text_input("Player 10")
-team1_player11 = st.text_input("Player 11")
-
-st.subheader("Enter Team 2 players:")
-
-team2_player1 = st.text_input("Opponent Player 1")
-team2_player2 = st.text_input("Opponent Player 2")
-team2_player3 = st.text_input("Opponent Player 3")
-team2_player4 = st.text_input("Opponent Player 4")
-team2_player5 = st.text_input("Opponent Player 5")
-team2_player6 = st.text_input("Opponent Player 6")
-team2_player7 = st.text_input("Opponent Player 7")
-team2_player8 = st.text_input("Opponent Player 8")
-team2_player9 = st.text_input("Opponent Player 9")
-team2_player10 = st.text_input("Opponent Player 10")
-team2_player11 = st.text_input("Opponent Player 11")
+team1_player1 = st.text_input("Opponent Player 1")
+team1_player2 = st.text_input("Opponent Player 2")
+team1_player3 = st.text_input("Opponent Player 3")
+team1_player4 = st.text_input("Opponent Player 4")
+team1_player5 = st.text_input("Opponent Player 5")
+team1_player6 = st.text_input("Opponent Player 6")
+team1_player7 = st.text_input("Opponent Player 7")
+team1_player8 = st.text_input("Opponent Player 8")
+team1_player9 = st.text_input("Opponent Player 9")
+team1_player10 = st.text_input("Opponent Player 10")
+team1_player11 = st.text_input("Opponent Player 11")
 
 torch.manual_seed(42) # set the random seed
 
-team1 = np.array([team1_player1, team2_player2, team1_player3, team1_player4, team1_player5, team1_player6, team1_player7, team1_player8, team1_player9, team1_player10, team1_player11])
+team0 = np.array([team0_player1, team1_player2, team0_player3, team0_player4, team0_player5, team0_player6, team0_player7, team0_player8, team0_player9, team0_player10, team0_player11])
 
-team2 = np.array([team2_player1, team2_player2, team2_player3, team2_player4, team2_player5, team2_player6, team2_player7, team2_player8, team2_player9, team2_player10, team2_player11])
+team1 = np.array([team1_player1, team1_player2, team1_player3, team1_player4, team1_player5, team1_player6, team1_player7, team1_player8, team1_player9, team1_player10, team1_player11])
 
-def get_features_dataset(team1, team2):
+def get_features_dataset(team0, team1):
     career_features_df = pd.read_excel('career_bat_bowl_combined.xlsx')
 
     numFeatures = 9	
@@ -96,10 +96,10 @@ def get_features_dataset(team1, team2):
     data_row = []
 
     # Get the 9 features for each player
-    for player in team1:
+    for player in team0:
         data_row = np.append(data_row, name_to_features_dict[player.strip()])
 
-    for player in team2:
+    for player in team1:
         data_row = np.append(data_row, name_to_features_dict[player.strip()])
 
     data.append(data_row)
@@ -205,8 +205,8 @@ def train(model, data, val_set, batch_size=64, num_epochs=1 , learning_rate = 0.
       print("Final Training Accuracy: {}".format(train_acc[-1]))
       print("Final Validation Accuracy: {}".format(val_acc[-1]))
 
-def winIndex(team1, team2):
-    test_data = get_features_dataset(team1, team2)
+def winIndex(team0, team1):
+    test_data = get_features_dataset(team0, team1)
     train_set, val_set = get_training_data()
 
     model = Primary_Model()
@@ -219,11 +219,11 @@ def winIndex(team1, team2):
 
     return prediction
 
-result = winIndex(team1, team2)
+result = winIndex(team0, team1)
 
 if (result == 0):
-    st.write('Team 1 wins')
+    st.write('Team 0 wins')
 elif (result == 1):
-    st.write('Team 2 wins')
+    st.write('Team 1 wins')
 else:
     st.write('No Result')
